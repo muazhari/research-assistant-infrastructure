aa076f463c2c68791853ad21c858b8f3d2315d38
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2024-08-02 11:28:39 +0700

    Update init.sql

--- migration/cockroach/init.sql
+++ migration/cockroach/init.sql
@@ -40,8 +40,8 @@ drop table if exists document_process cascade;
 create table document_process
 (
     id                  uuid primary key,
-    initial_document_id uuid unique not null references document (id) on delete cascade on update cascade,
-    final_document_id   uuid unique not null references document (id) on delete cascade on update cascade,
+    initial_document_id uuid        not null references document (id) on delete cascade on update cascade,
+    final_document_id   uuid        not null references document (id) on delete cascade on update cascade,
     started_at          timestamptz not null,
     finished_at         timestamptz not null,
     constraint initial_and_final_document_id_different_check check (initial_document_id != final_document_id)
